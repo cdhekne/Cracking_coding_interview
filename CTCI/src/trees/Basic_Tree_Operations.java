ff7b1def0a4294ff7ed4ec1ca23f1d9e8f66ec7e
cdhekne <chinmaydhekne@gmail.com>
2016-01-30 16:51:47 -0700

    Adding logic to add new Node in tree

--- CTCI/src/trees/Basic_Tree_Operations.java
+++ CTCI/src/trees/Basic_Tree_Operations.java
@@ -7,13 +7,61 @@ public class Basic_Tree_Operations {
 	public static void main(String []args){
 		Basic_Tree_Operations tree_Operations = new Basic_Tree_Operations();
 		
-		tree_Operations.addElementToTree(12);
+		tree_Operations.addElementToTree(50);
+		tree_Operations.addElementToTree(30);
+		tree_Operations.addElementToTree(60);
+		tree_Operations.addElementToTree(15);
+		tree_Operations.addElementToTree(45);
+//		tree_Operations.printTree();
 		
 		
-		tree_Operations.findElementInBST(12);
+		
+		
+//		tree_Operations.findElementInBST(12);
+	}
+	private void printTree() {
+		Node currentNode = root;
+		while(currentNode!=null){
+			
+		}
 	}
 	private void addElementToTree(int dataToAdd) {
+		Node temp = new Node(dataToAdd);
+		Node currentNode = root;
 		
+		if(currentNode==null){ //New Node is the root of the tree
+			currentNode = temp;
+			currentNode.left=null;
+			currentNode.right = null;
+			root = currentNode;
+		}
+		else{	//Go to correct place for insertion
+			while(currentNode.left!=null && currentNode.right!=null)
+			{
+				if(dataToAdd>currentNode.data)
+					currentNode= currentNode.right;
+				
+				else if(dataToAdd<currentNode.data)
+					currentNode = currentNode.left;
+
+				else if(dataToAdd==currentNode.data)
+				{
+					System.out.println("Duplicate data found. Insertion not possible !");
+					System.exit(0);
+				}
+			}
+			if(dataToAdd<currentNode.data){	//insert to left
+				currentNode.left= temp; 
+				currentNode = currentNode.left;
+			}
+			else if(dataToAdd>currentNode.data){	//insert to right
+				currentNode.right= temp;
+				currentNode = currentNode.right;
+			}
+			currentNode.left = null;
+			currentNode.right= null;
+			
+		}
 	}
 	private void findElementInBST(int dataToSearch) {
 		Node currentNode = root;
